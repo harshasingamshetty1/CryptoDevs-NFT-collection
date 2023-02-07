a893b6425e336924c82981646ab06bd1dbd3ce31
harshasingamshetty1 <harshasingamshetty1@gmail.com>
2023-02-08 01:01:15 +0530

    modify metadata url

--- contracts-hardhat/constants/index.ts
+++ contracts-hardhat/constants/index.ts
@@ -1,6 +1,7 @@
 // Address of the Whitelist Contract that you deployed
 const WHITELIST_CONTRACT_ADDRESS = "0xB5fA8E539da47cee5a57116F9E8B7Fb92a585BC6";
 // URL to extract Metadata for a Crypto Dev NFT
-const METADATA_URL = "https://crypto-devs-nft-collection-delta.vercel.app/api/";
+const METADATA_URL =
+  "https://crypto-devs-nft-collection-clwjabndq-harshasingamshetty1.vercel.app/api";
 
 export { WHITELIST_CONTRACT_ADDRESS, METADATA_URL };
