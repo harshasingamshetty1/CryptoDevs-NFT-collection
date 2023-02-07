734554771e9cc85a27efe86bcb3fac0334619041
harshasingamshetty1 <harshasingamshetty1@gmail.com>
2023-02-08 01:11:51 +0530

    latest contract address

--- contracts-hardhat/constants/index.ts
+++ contracts-hardhat/constants/index.ts
@@ -1,7 +1,6 @@
 // Address of the Whitelist Contract that you deployed
-const WHITELIST_CONTRACT_ADDRESS = "0xB5fA8E539da47cee5a57116F9E8B7Fb92a585BC6";
+const WHITELIST_CONTRACT_ADDRESS = "0xBBC8316551999430cb9072987bE0ba9cC917aFc6";
 // URL to extract Metadata for a Crypto Dev NFT
-const METADATA_URL =
-  "https://crypto-devs-nft-collection-clwjabndq-harshasingamshetty1.vercel.app/api/";
+const METADATA_URL = "https://crypto-devs-nft-collection-delta.vercel.app/api/";
 
 export { WHITELIST_CONTRACT_ADDRESS, METADATA_URL };
